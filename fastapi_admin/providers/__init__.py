6374355bcba6fd09ab25b25971aefe22665166bb
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-06-22 11:07:18 +0100

    Update __init__.py

--- fastapi_admin/providers/__init__.py
+++ fastapi_admin/providers/__init__.py
@@ -1,1 +1,8 @@
- 
+ from abc import ABC, abstractmethod
+from fastapi_admin.app import FastAPIAdmin
+
+
+class Provider(ABC):
+    @abstractmethod
+    async def register(self, app: FastAPIAdmin):
+        pass
