b5a803d5f350aef7a6f3a9241788298c3450bba6
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-06-25 11:13:32 +0100

    Update template.py

--- fastapi_admin/template.py
+++ fastapi_admin/template.py
@@ -7,30 +7,27 @@ env: Environment = Environment(
     autoescape=select_autoescape(["html", "xml"]),
 )
 
-
 def add_template_folder(*template_folders: str):
     global env
     env.loader = FileSystemLoader(list(template_folders))
-
 
 def template_global(name: str):
     def wrapper(func):
         env.globals[name] = func
         return func
-
     return wrapper
-
 
 def template_filter(name: str = None):
     def wrapper(func):
         env.filters[name or func.__name__] = func
         return func
-
     return wrapper
-
 
 def render(template_name: str, context: Any = None):
     if context is None:
         context = {}
     template = env.get_template(template_name)
     return template.render(context)
+
+# ✅ Fix for external imports expecting `templates`
+templates = env
